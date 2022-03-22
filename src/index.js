17c61d0b19da8b1c29d883cdaeee0af5f3a53735
junzzi <jjh01027717502@gmail.com>
2022-03-22 12:24:51 +0900

    설계완료

--- src/index.js
+++ src/index.js
@@ -1,51 +1,41 @@
+// VendingMachine {
+// 	IceAmericanoCount,
+//   hotAmericanoCount,
+//   todos
+// }
+
+// class VendingMachine {
+// 	iceAmericanoCount;
+// 	hotAmericanoCount;
+// 	todos;
+// }
+
 /**
- * Represents a book.
+ * 자판기 객체를 생성하는 함수
  * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @param {number} IceAmericanoCount - 가지고 있는 아이스 아메리카노 갯수
+ * @param {number} hotAmericanoCount - 가지고 있는 따듯한 아메리카노 갯수
  */
-function Book(title, author) {}
+function VendingMachine(iceAmericanoCount, hotAmericanoCount) {
+  /** @member {number} iceAmericanoCount 차가운 아메리카노의 갯수 */
+  this.iceAmericanoCount;
+  /** @member {number} hotAmericanoCount 따듯한 아메리카노의 갯수 */
+  this.hotAmericanoCount;
+  /** @member {Array} todos 로그들이 저장되는 배열*/
+  this.todos;
+}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
+ * 금액을 넣으면 아메리카노를 구입하는 로그를 볼 수 있다.
+ * @param {number} inputMoney - 자판기에 투입한 금액
  * @return {boolean} Always returns false.
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+VendingMachine.prototype.getItem = function (inputMoney) {};
+
+/**
+ * 자판기에 아메리카노를 추가하는 메서드
+ * @param {string} americanoType - 자판기에 투입한 금액
+ * @param {number} inputCount - 추가하는 상품의 갯수
+ * @return {boolean} Always returns false.
+ */
+VendingMachine.prototype.addItem = function (americanoType, inputCount) {};
